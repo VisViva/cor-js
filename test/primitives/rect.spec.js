1cf9f68ad1af48f35f8f96ced84285203f21f9ae
AGasimov <agasimov@luxoft.com>
2016-11-04 20:24:28 +0300

    #20 Additional test case

--- test/primitives/rect.spec.js
+++ test/primitives/rect.spec.js
@@ -205,5 +205,20 @@ describe('Rect tests', () => {
             expect(rect.bboxCascaded().width()).to.be.approximately(14.1, 0.1);
             expect(rect.bboxCascaded().height()).to.be.approximately(14.1, 0.1);
         });
+
+        it('Gets scaled, rotated and translated primitives bounding box with an offset correctly', () => {
+            rect = new Rect().at(10, 10).translate(10, 10).rotate(270).width(10).height(10).cascade();
+            bbox = rect.bboxCascaded();
+            expect(bbox.x()).to.be.approximately(-4.9, 0.1);
+            expect(bbox.y()).to.be.approximately(4.9, 0.1);
+            expect(bbox.width()).to.be.approximately(10.0, 0.1);
+            expect(bbox.height()).to.be.approximately(10.1, 0.1);
+            rect.translate(10, 10).rotate(45).cascade();
+            bbox = rect.bboxCascaded();
+            expect(rect.bboxCascaded().x()).to.be.approximately(13, 0.1);
+            expect(rect.bboxCascaded().y()).to.be.approximately(-7.0, 0.1);
+            expect(rect.bboxCascaded().width()).to.be.approximately(14.1, 0.1);
+            expect(rect.bboxCascaded().height()).to.be.approximately(14.1, 0.1);
+        });
     });
 });
