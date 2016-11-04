4e8dc70a82a188a7be46347699ecd1abea2db84b
Vis Viva <a.vis.viva@gmail.com>
2016-11-05 01:04:41 +0300

    #g Refactored chart sample code

--- samples/chart.js
+++ samples/chart.js
@@ -37,23 +37,23 @@ const scene_manager = new SceneManager();
     }
 
     function drawGrid() {
-        for (let i = 0; i < 11; ++i) {
+        for (let i = -5; i < 6; ++i) {
             const line = new Path();
-            line.debug(true).depth(-100).at(0, i * scene._canvas.height * 4 / 60).linearTo(scene._canvas.width * 4 / 6, i * scene._canvas.height * 4 / 60).material(gridMaterial);
+            line.depth(-100).at(- scene._canvas.width / 3, i * scene._canvas.height * 4 / 60).linearTo(scene._canvas.width / 3, i * scene._canvas.height * 4 / 60).material(gridMaterial);
             root.append(line);
         }
-        for (let i = 0; i < 11; ++i) {
+        for (let i = -5; i < 6; ++i) {
             const line = new Path();
-            line.debug(true).depth(-100).at(i * scene._canvas.width * 4 / 60, 0).linearTo(i * scene._canvas.width * 4 / 60, scene._canvas.height * 4 / 6).material(gridMaterial);
+            line.depth(-100).at(i * scene._canvas.width * 4 / 60, - scene._canvas.height / 3).linearTo(i * scene._canvas.width * 4 / 60, scene._canvas.height / 3).material(gridMaterial);
             root.append(line);
         }
     }
 
     function drawBars() {
-        for (let i = 0; i < 5; ++i) {
+        for (let i = -2; i < 3; ++i) {
             const bar = new Rect();
             var height = scene._canvas.height / (Math.random() * 7 + 2);
-            bar.debug(true).depth(i).material(barMaterial).at(i * scene._canvas.width * 4 / 30 + scene._canvas.width * 2 / 30, - scene._canvas.height * 4 / 6 + height / 2).width(scene._canvas.width / 30).height(height);
+            bar.debug(true).depth(i).material(barMaterial).at(i * scene._canvas.width * 4 / 30, - scene._canvas.height / 3 + height / 2).width(scene._canvas.width / 30).height(height);
             root.append(bar);
         }
     }
@@ -62,6 +62,5 @@ const scene_manager = new SceneManager();
     drawBars();
 
     scene.start(() => {
-      root.rotate(0.01);
     });
 }();
