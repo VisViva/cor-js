247f1f215ff15bca30ccb1ccefb01969b4ec5f24
Vis Viva <a.vis.viva@gmail.com>
2016-11-06 00:23:49 +0300

    #15 Added timeline sample

---
+++ samples/timeline.js
@@ -0,0 +1,41 @@
+import {
+    SceneManager
+} from '../src/scene_manager';
+import {
+    random_color
+} from '../src/utils/helper';
+
+const scene_manager = new SceneManager();
+const scene = scene_manager.new('scene');
+const root = scene.root();
+const Rect = scene.factory().Rect;
+const rect = new Rect();
+
+window.addEventListener('resize', function(event) {
+    scene.resize();
+});
+
+scene
+    .grid(true)
+    .fps(60)
+    .material()
+    .stroke('#000000')
+    .width(1)
+    .fill('#CCCCCC');
+
+scene.render();
+
+rect
+    .debug(true)
+    .width(100)
+    .height(100)
+    .material()
+    .fill(random_color());
+
+root
+    .timed(true)
+    .append(rect);
+
+
+scene.start(() => {
+});
