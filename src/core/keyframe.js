3a61e9da9cd7c0007727cb228af58d8c9cb6fe1e
Vis Viva <a.vis.viva@gmail.com>
2016-11-06 16:19:58 +0300

    #15 Rethinking  keyframe appending logic

--- src/core/keyframe.js
+++ src/core/keyframe.js
@@ -5,7 +5,6 @@
  */
 
 function Keyframe() {
-    this._node = null;
     this._time = 0;
     this._keys = {};
 };
@@ -24,26 +23,13 @@ Keyframe.prototype.time = function(time) {
 };
 
 /**
- * Get or set node, the keyframe relates to
- */
-
-Keyframe.prototype.node = function(node) {
-    if (node) {
-        this._node = node;
-        return this;
-    } else {
-        return this._node;
-    }
-};
-
-/**
  * Get or set position keys
  */
 
 Keyframe.prototype.position = function(x, y) {
     if (typeof x !== 'undefined' && typeof y !== 'undefined') {
-        this._keys.position_x = x;
-        this._keys.position_y = y;
+        this.position_x(x);
+        this.position_y(y);
         return this;
     } else {
         return {
@@ -59,7 +45,8 @@ Keyframe.prototype.position = function(x, y) {
 
 Keyframe.prototype.position_x = function(x) {
     if (typeof x !== 'undefined') {
-        this._keys.position_x = x;
+        if (x === null) delete this._keys.position_y;
+        else this._keys.position_x = x;
         return this;
     } else {
         return this._keys.position_x;
@@ -72,45 +59,11 @@ Keyframe.prototype.position_x = function(x) {
 
 Keyframe.prototype.position_y = function(y) {
     if (typeof y !== 'undefined') {
-        this._keys.position_y = y;
+        if (x === null) delete this._keys.position_y;
+        else this._keys.position_y = y;
         return this;
     } else {
         return this._keys.position_y;
-    }
-};
-
-/**
- * Delete keyframe keys
- */
-
-Keyframe.prototype.remove = {
-
-    /**
-     * Delete position keys
-     */
-
-    position: function() {
-        delete this._keys.position_x;
-        delete this._keys.position_y;
-        return this;
-    },
-
-    /**
-     * Delete position keys
-     */
-
-    position_x: function() {
-        delete this._keys.position_x;
-        return this;
-    },
-
-    /**
-     * Delete position keys
-     */
-
-    position_y: function() {
-        delete this._keys.position_y;
-        return this;
     }
 };
 
--- src/core/timeline.js
+++ src/core/timeline.js
@@ -5,6 +5,59 @@
  */
 
 function Timeline() {
+    this._nodes = [node];
+    this._tracks = [
+        [{
+            position_x: [
+                0: 0,
+                1000: 100
+            ],
+            position_y: [
+                0: 0,
+                5000: 50
+            ]
+        }]
+    ];
+};
+
+/**
+ * Add keyframe
+ */
+
+Timeline.prototype.add = function(node, keyframe) {
+
+    /**
+     * Track reference
+     */
+
+    let track;
+
+    /**
+     * Acquire correct track reference, bound to the given node
+     */
+
+    const index = this._nodes.indexOf(node);
+    if (index === -1) {
+        this._nodes.push(node);
+        track = {};
+        this._tracks.push(track);
+    } else {
+        track = this._tracks[index];
+    }
+
+    /**
+     * Add given keyframe to the track
+     */
 };
 
 exports.Timeline = Timeline;
+
+/* get first 2 element of an array
+
+var i = 0;
+for (var property in a) {
+    if (a.hasOwnProperty(property)) {
+         console.log(property);
+         ++i; if (i === 2) break;
+    }
+}
