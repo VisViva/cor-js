b44e7ab85173ba4f120212dd6e7924151df9e52e
Vis Viva <a.vis.viva@gmail.com>
2016-11-11 00:32:31 +0300

    #g Fixed demo and a timeline bug regarding easing call

--- samples/materials.js
+++ samples/materials.js
@@ -17,45 +17,46 @@ scene
     .material()
     .stroke('#000000')
     .width(1)
-    .fill('#CCCCCC');
+    .fill('#000000');
 
 window.addEventListener('resize', function(event) {
     scene.resize();
 });
 
-for (let i = -25; i < 26; ++i) {
-    for (let j = -18; j < 19; ++j) {
+for (let i = -28; i < 29; ++i) {
+    for (let j = -28; j < 29; ++j) {
         const rect = new Rect();
         const path = new Path();
         rect
             .debug(false)
-            .translate(150 * i, 150 * j)
             .width(100)
             .height(100)
             .rotate(i * j)
             .pivot(10*i, 10*j)
             .material()
-            .width(Math.abs(i))
+            .width(Math.abs(i)/2)
             .stroked(true)
             .filled(false)
-            .stroked(random_color());
+            .stroke('#00FF00')
+            .fill(random_color());
         root
             .append(rect);
 
         scene.timeline().add(
             rect,
-            new Keyframe().rotate(0, 'linear', 'linear').scale(1, 1).translateX(-200*i),
-            new Keyframe().time(Math.random() * 1000 + 100*(Math.abs(i)+1)*(Math.abs(j)+1)).scale(Math.random() * 2, Math.random() * 2, 'elastic', 'linear').rotate(Math.random() * 5 * Math.abs(i)*Math.abs(j), 'elastic', 'linear'),
-            new Keyframe().time(Math.random() * 1000 + 150*(Math.abs(i)+1)*(Math.abs(j)+1)).scale(Math.random() * 3, Math.random() * 3, 'elastic', 'linear').rotate(Math.random() * 10 * Math.abs(i)*Math.abs(j), 'elastic', 'linear'),
-            new Keyframe().time(Math.random() * 1000 + 200*(Math.abs(i)+1)*(Math.abs(j)+1)).scale(Math.random() * 4, Math.random() * 4, 'elastic', 'linear').rotate(Math.random() * 15 * Math.abs(i)*Math.abs(j), 'elastic', 'linear').translateX(200*i, 'elastic', 'linear')
+            new Keyframe().rotate(0).scale(1, 1).translate(150 * i, 150 * j),
+            new Keyframe().time(1000 + 100*(Math.abs(i))*(Math.abs(j))).scale(Math.random() * 2, Math.random() * 2, 'quad', 'quad').rotate(5 * i, 'quad', 'quad'),
+            new Keyframe().time(1000 + 150*(Math.abs(i))*(Math.abs(j))).scale(Math.random() * 3, Math.random() * 3, 'quad', 'quad').rotate(Math.random() * 10 * j, 'quad', 'quad'),
+            new Keyframe().time(1000 + 200*(Math.abs(i))*(Math.abs(j))).scale(Math.random() * 4, Math.random() * 4, 'quad', 'quad').rotate(Math.random() * 15 * i, 'quad', 'quad').translate(200 * i, 200 * j, 'elastic', 'linear')
         );
     }
 }
 
 scene.timeline().add(
     root,
-    new Keyframe().scale(0.66, 0.66),
-    new Keyframe().time(30000).scale(0.16, 0.16, 'quad', 'linear').rotate(180, 'quad', 'linear')
+    new Keyframe().scale(0, 0),
+    new Keyframe().time(25000).scale(0.2, 0.2, 'quad', 'quad').rotate(90, 'quad', 'quad'),
+    new Keyframe().time(50000).scale(0, 0, 'quad', 'quad')
 );
 
 scene.start();
--- samples/timeline.js
+++ samples/timeline.js
@@ -29,7 +29,7 @@ scene.render();
 rect
     .width(100)
     .height(100)
-    .debug(true)
+    .debug(false)
     .material()
     .fill(random_color());
 
@@ -38,10 +38,9 @@ root
 
 scene.timeline().add(
     rect,
-    new Keyframe().rotate(0, 'linear', 'linear').scale(1, 1).translateX(-200),
-    new Keyframe().time(1000).scale(1.1, 3.5, 'elastic', 'linear').rotate(90, 'bounce', 'linear').translateX(200, 'bounce', 'linear'),
-    new Keyframe().time(2000).scale(2, 2, 'elastic', 'linear').rotate(45, 'bounce', 'linear'),
-    new Keyframe().time(4000).scale(3, 3, 'elastic', 'linear').rotate(90, 'bounce', 'linear')
+    new Keyframe().scale(0, 0),
+    new Keyframe().time(1000).scale(0.2, 0.2, 'quad', 'linear').rotate(90, 'quad', 'linear'),
+    new Keyframe().time(4000).scale(0, 0, 'quad', 'linear')
 );
 
 scene.start();
