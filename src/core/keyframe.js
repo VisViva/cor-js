ad129b86582f897ac6a62077f7ee1def61752c75
AGasimov <agasimov@luxoft.com>
2016-11-10 17:49:00 +0300

    #15 Implemented simple primitive keyframes

--- src/core/keyframe.js
+++ src/core/keyframe.js
@@ -43,12 +43,12 @@ Keyframe.prototype.translate = function(x, y, ease_in, ease_out) {
  * Get or set the translation key on the x axis
  */
 
-Keyframe.prototype.translateX = function(x, ease_in, ease_out) {
-    if (x !== undefined) {
-        if (x === null) delete this._keys.translateX;
+Keyframe.prototype.translateX = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.translateX;
         else {
             this._keys.translateX = {
-                value: x,
+                value: value,
                 ease_in: ease_in || this._keys.translateX && this._keys.translateX.ease_in || 'linear',
                 ease_out: ease_out || this._keys.translateX && this._keys.translateX.ease_out || 'linear'
             };
@@ -63,12 +63,12 @@ Keyframe.prototype.translateX = function(x, ease_in, ease_out) {
  * Get or set the translation key on the y axis
  */
 
-Keyframe.prototype.translateY = function(y, ease_in, ease_out) {
-    if (y !== undefined) {
-        if (y === null) delete this._keys.translateY;
+Keyframe.prototype.translateY = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.translateY;
         else {
             this._keys.translateY = {
-                value: y,
+                value: value,
                 ease_in: ease_in || this._keys.translateY && this._keys.translateY.ease_in || 'linear',
                 ease_out: ease_out || this._keys.translateY && this._keys.translateY.ease_out || 'linear'
             };
@@ -83,12 +83,12 @@ Keyframe.prototype.translateY = function(y, ease_in, ease_out) {
  * Get or set rotation key
  */
 
-Keyframe.prototype.rotate = function(rotation, ease_in, ease_out) {
-    if (rotation !== undefined) {
-        if (rotation === null) delete this._keys.rotate;
+Keyframe.prototype.rotate = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.rotate;
         else {
             this._keys.rotate = {
-                value: rotation,
+                value: value,
                 ease_in: ease_in || this._keys.rotate && this._keys.rotate.ease_in || 'linear',
                 ease_out: ease_out || this._keys.rotate && this._keys.rotate.ease_out || 'linear'
             };
@@ -120,12 +120,12 @@ Keyframe.prototype.scale = function(x, y, ease_in, ease_out) {
  * Get or set the scale key on the x axis
  */
 
-Keyframe.prototype.scaleX = function(x, ease_in, ease_out) {
-    if (x !== undefined) {
-        if (x === null) delete this._keys.scaleX;
+Keyframe.prototype.scaleX = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.scaleX;
         else {
             this._keys.scaleX = {
-                value: x,
+                value: value,
                 ease_in: ease_in || this._keys.scaleX && this._keys.scaleX.ease_in || 'linear',
                 ease_out: ease_out || this._keys.scaleX && this._keys.scaleX.ease_out || 'linear'
             };
@@ -140,12 +140,12 @@ Keyframe.prototype.scaleX = function(x, ease_in, ease_out) {
  * Get or set the scale key on the y axis
  */
 
-Keyframe.prototype.scaleY = function(y, ease_in, ease_out) {
-    if (y !== undefined) {
-        if (y === null) delete this._keys.scaleY;
+Keyframe.prototype.scaleY = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.scaleY;
         else {
             this._keys.scaleY = {
-                value: y,
+                value: value,
                 ease_in: ease_in || this._keys.scaleY && this._keys.scaleY.ease_in || 'linear',
                 ease_out: ease_out || this._keys.scaleY && this._keys.scaleY.ease_out || 'linear'
             };
@@ -153,6 +153,66 @@ Keyframe.prototype.scaleY = function(y, ease_in, ease_out) {
         return this;
     } else {
         return this._keys.scaleY;
+    }
+};
+
+/**
+ * Get or set the width key
+ */
+
+Keyframe.prototype.width = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.width;
+        else {
+            this._keys.width = {
+                value: value,
+                ease_in: ease_in || this._keys.width && this._keys.width.ease_in || 'linear',
+                ease_out: ease_out || this._keys.width && this._keys.width.ease_out || 'linear'
+            };
+        }
+        return this;
+    } else {
+        return this._keys.width;
+    }
+};
+
+/**
+ * Get or set the width key
+ */
+
+Keyframe.prototype.height = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.height;
+        else {
+            this._keys.height = {
+                value: value,
+                ease_in: ease_in || this._keys.height && this._keys.height.ease_in || 'linear',
+                ease_out: ease_out || this._keys.height && this._keys.height.ease_out || 'linear'
+            };
+        }
+        return this;
+    } else {
+        return this._keys.height;
+    }
+};
+
+/**
+ * Get or set the width key
+ */
+
+Keyframe.prototype.radius = function(value, ease_in, ease_out) {
+    if (value !== undefined) {
+        if (value === null) delete this._keys.radius;
+        else {
+            this._keys.radius = {
+                value: value,
+                ease_in: ease_in || this._keys.radius && this._keys.radius.ease_in || 'linear',
+                ease_out: ease_out || this._keys.radius && this._keys.radius.ease_out || 'linear'
+            };
+        }
+        return this;
+    } else {
+        return this._keys.radius;
     }
 };
 
